621a3673e01e8fb585ef294b726b9556128f889b
NA.Rukhlov <ruhlovnikita@gmail.com>
2017-06-21 21:43:23 +0300

    Add my line

--- src/main/java/com/acme/edu/App.java
+++ src/main/java/com/acme/edu/App.java
@@ -4,10 +4,10 @@ package com.acme.edu;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main( String[] args ) {
         System.out.println( "Hello World!" );
+        System.out.print( "Hello" );
+        System.out.println( " from Nikita" );
     }
 }
