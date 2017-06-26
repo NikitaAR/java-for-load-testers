19e9b9f9eac1064c1e27b792e927d6ade395792f
NA.Rukhlov <ruhlovnikita@gmail.com>
2017-06-26 17:04:07 +0300

    Calculator 1.1

---
+++ src/main/java/com/acme/edu/com/jet/edu/Calculator.java
@@ -0,0 +1,68 @@
+package com.acme.edu.com.jet.edu;
+
+/**
+ * Created by NA.Rukhlov on 26.06.2017.
+ */
+public class Calculator {
+    private static int state = 0;
+    private static int mem = 0;
+
+    public static int add(int a, int b) {
+        state = a + b;
+        return state;
+    }
+
+    public static int sub(int a, int b) {
+        state = a - b;
+        return state;
+    }
+
+    public static int mul(int a, int b) {
+        state = a * b;
+        return state;
+    }
+
+    public static int div(int a, int b) {
+        state = a / b;
+        return state;
+    }
+
+    public static void addMem() {
+        mem = mem + state;
+    }
+
+    public static void clearMem() {
+        mem = 0;
+    }
+    public static int showState(){
+        return state;
+    }
+    public static int showMem(){
+        return mem;
+    }
+}
+
+class CalculatorApp {
+    public static void main(String[] args) {
+        // add, sub
+        System.out.println(Calculator.add(3, 1));
+        System.out.println(Calculator.sub(4, 2));
+
+        Calculator.addMem();
+
+        //mul, div
+        System.out.println(Calculator.mul(2, 3));
+        System.out.println(Calculator.div(6, 6));
+
+        //mem
+        System.out.println(Calculator.showMem());
+        Calculator.clearMem();
+        System.out.println(Calculator.showMem());
+
+        //state
+        Calculator.mul(5, 5);
+        System.out.println("last state = " + Calculator.showState());
+
+    }
+
+}
