1b9d92d98aaa55ea7b3e83254d7ec2e782a3aa35
NA.Rukhlov <ruhlovnikita@gmail.com>
2017-06-30 13:53:27 +0300

    Calculator for double

--- src/main/java/com/acme/edu/com/jet/edu/Calculator.java
+++ src/main/java/com/acme/edu/com/jet/edu/Calculator.java
@@ -1,64 +1,131 @@
 package com.acme.edu.com.jet.edu;
 
+import static com.acme.edu.com.jet.edu.Calculator.*;
+
 /**
- * Created by NA.Rukhlov on 26.06.2017.
+ * Class Calculator for Int and Double types, values from -10 to 10
  */
 public class Calculator {
-    private static int state = 0;
-    private static int mem = 0;
+    private static double state = 0;
+    private static double mem = 0;
 
-    public static int add(int a, int b) {
+     public static double add(int a, int b) {
         state = a + b;
+        state = getValidatedInterval(state);
         return state;
     }
 
-    public static int sub(int a, int b) {
+    public static double sub(int a, int b) {
         state = a - b;
+        state = getValidatedInterval(state);
         return state;
     }
 
-    public static int mul(int a, int b) {
+    public static double mul(int a, int b) {
         state = a * b;
+        state = getValidatedInterval(state);
         return state;
     }
 
-    public static int div(int a, int b) {
+    public static double div(int a, int b) {
         state = a / b;
+        state = getValidatedInterval(state);
         return state;
+    }
+
+    public static double add(double a, double b) {
+        state = a + b;
+        state = getValidatedInterval(state);
+        return state;
+    }
+
+    public static double sub(double a, double b) {
+        state = a - b;
+        state = getValidatedInterval(state);
+        return state;
+    }
+
+    public static double mul(double a, double b) {
+        double result = getValidatedInterval(a * b);
+        state = result;
+        return state;
+    }
+
+    public static double div(double a, double b) {
+        state = a / b;
+        state = getValidatedInterval(state);
+        return state;
+    }
+
+    public static double absolute(int a){
+        if(a < 0){
+            a = -a;
+        }
+        state = a;
+        getValidatedInterval(state);
+        return state;
+    }
+    public static double absolute(double a){
+        if(a < 0){
+            a = -a;
+        }
+        state = a;
+        getValidatedInterval(state);
+        return state;
+    }
+
+
+    public static double getValidatedInterval(double value){
+        if(value > 10) value = 10;
+        else if(value < -10) value = -10;
+        return value;
     }
 
     public static void addMem() {
         mem = mem + state;
     }
 
-    public static void clearMem() {
+    public static void clearMemory() {
         mem = 0;
     }
-    public static int showState(){
+    public static double getState(){
         return state;
     }
-    public static int showMem(){
+    public static double getMem(){
         return mem;
     }
 }
 
 class CalculatorApp {
     public static void main(String[] args) {
-        
-        System.out.println(Calculator.add(3, 1));
-        System.out.println(Calculator.sub(4, 2));
 
-        Calculator.addMem();
+        System.out.println(add(3, 1));
+        System.out.println(sub(4, 2));
 
-        System.out.println(Calculator.mul(2, 3));
-        System.out.println(Calculator.div(6, 6));
+        addMem();
 
-        System.out.println(Calculator.showMem());
-        Calculator.clearMem();
-        System.out.println(Calculator.showMem());
+        System.out.println(mul(2, 3));
+        System.out.println(div(6, 6));
 
-        Calculator.mul(5, 5);
-        System.out.println("last state = " + Calculator.showState());
+        System.out.println(getMem());
+        clearMemory();
+        System.out.println(getMem());
+
+        mul(5, 5);
+        System.out.println("last state = " + getState());
+
+
+        System.out.println(add(1.1, 1));
+        System.out.println(sub(4, 2.1));
+
+        System.out.println(mul(2.5, 6));
+        System.out.println(div(-3, 6));
+
+        System.out.println(mul(-2.5, 6));
+
+        System.out.println(absolute(-5));
+
+
 
     }
 
